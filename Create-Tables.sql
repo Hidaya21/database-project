7dcb55d1a664b17386dae9954d23dfb1e5261ab4
Hidaya21 <140381535+Hidaya21@users.noreply.github.com>
2026-05-04 09:18:08 +0400

    Create Tables

---
+++ Create-Tables.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE project
+CREATE TABLE airports (
+    IATA VARCHAR(3) PRIMARY KEY,
+    names VARCHAR(100),
+    city VARCHAR(100),
+    country VARCHAR(100)
+);
+
+CREATE TABLE aircraft (
+    aircraft_ID INT PRIMARY KEY,
+    registration_number VARCHAR(20) UNIQUE,
+    model VARCHAR(50),
+    manufacture VARCHAR(50),
+    capacity INT,
+    year_of_manufactur INT
+);
+
+
+CREATE TABLE flights (
+    flights_ID INT PRIMARY KEY,
+    flight_number VARCHAR(10),
+    departure_datetime DATETIME,
+    arrival_datetime DATETIME,
+    status1 VARCHAR(20) CHECK (status1 IN ('Scheduled', 'Delayed', 'Cancelled', 'Completed')),
+    O_IATA VARCHAR(3),
+    D_IATA VARCHAR(3),
+    aircraft_ID INT,
+    FOREIGN KEY (O_IATA) REFERENCES airports(IATA),
+    FOREIGN KEY (D_IATA) REFERENCES airports(IATA),
+    FOREIGN KEY (aircraft_ID) REFERENCES aircraft(aircraft_ID)
+);
+
+CREATE TABLE Passenger (
+    national_ID VARCHAR(20) PRIMARY KEY,
+    names VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    nationality VARCHAR(50),
+    BD DATE
+);
+
+CREATE TABLE Crew (
+    Crew_ID INT PRIMARY KEY,
+    license VARCHAR(20) UNIQUE,
+    name1 VARCHAR(100),
+    role1 VARCHAR(20) CHECK (role1 IN ('Pilot', 'Co-Pilot', 'Flight Attendant', 'Engineer'))
+);
+
+CREATE TABLE booking (
+    booking_ID INT PRIMARY KEY,
+    class VARCHAR(15) CHECK (class IN ('Economy', 'Business', 'First')),
+    seat_num VARCHAR(5),
+    price DECIMAL(10, 2),
+    booking_date DATE,
+    national_ID VARCHAR(20),
+    flights_ID INT,
+    FOREIGN KEY (national_ID) REFERENCES Passenger(national_ID),
+    FOREIGN KEY (flights_ID) REFERENCES flights(flights_ID)
+);
+
+CREATE TABLE FlightCrew (
+    Crew_ID INT,
+    flights_ID INT,
+    PRIMARY KEY (Crew_ID, flights_ID),
+    FOREIGN KEY (Crew_ID) REFERENCES Crew(Crew_ID),
+    FOREIGN KEY (flights_ID) REFERENCES flights(flights_ID)
+);
